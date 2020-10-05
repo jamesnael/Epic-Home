4e43cd859732cc4f6b76cb9dae641a1eb18e335a
Toni Oktoro - Total IT <toni@totalit.co.id>
2020-10-05 13:35:47 +0700

    Breadcrumbs

---
+++ Modules/Core/Resources/views/components/breadcrumbs.blade.php
@@ -0,0 +1,22 @@
+<v-card
+    outlined>
+    <v-card-title>
+        <h3>{{ $breadcrumb_title }}</h3>
+    </v-card-title>
+
+    <v-card-subtitle>
+        <v-breadcrumbs
+            class="px-0 py-2" 
+            :items='@json($breadcrumbs)'>
+            <template v-slot:item="{ item }">
+                <v-breadcrumbs-item
+                    :href="item.href"
+                    :disabled="item.disabled"
+                >
+                    <v-icon v-if="item.text == 'mdi-home'" v-text="item.text"></v-icon>
+                    <span v-else>@{{ item.text }}</span>
+                </v-breadcrumbs-item>
+            </template>
+        </v-breadcrumbs>
+    </v-card-subtitle>
+</v-card>
